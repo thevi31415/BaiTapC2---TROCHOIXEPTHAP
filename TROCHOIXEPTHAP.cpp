f3a71d69f5c6a5423209b1830485b193a3d0355c
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2021-11-29 12:27:35 +0700

    first commit

---
+++ TROCHOIXEPTHAP.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+using namespace std;
+void xepthap(int n, int k);
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    xepthap(n, k);
+}
+void xepthap(int n, int k) {
+    int sl =0;
+    int t = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sl = sl + i;
+    }
+    for (int i = 1; k > 0; i++)
+    {
+        k = k - i;
+        if (k >= 0){
+            t = t + 1;
+        }
+    }
+    cout << sl << " ";
+    cout << t;
+  
+}
